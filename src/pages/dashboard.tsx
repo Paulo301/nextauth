24087b7fd9e526ce09f94dc1ca0d31e42a2d2bd5
Paulo301 <paulovictor1918@gmail.com>
2023-06-21 12:05:22 -0300

    Criando componente de permissão

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,19 +1,18 @@
+import { Can } from "@/components/Can";
 import { AuthContext } from "@/contexts/AuthContext";
-import { useCan } from "@/hooks/useCan";
 import { setupAPIClient } from "@/services/api";
 import { withSSRAuth } from "@/utils/withSSRAuth";
 import { useContext } from "react";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-  const userCanSeeMetrics = useCan({
-    permissions: ['metrics.create']
-  });
 
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-      { userCanSeeMetrics && <div>Métricas</div> }
+      <Can permissions={['metrics.list3']}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
